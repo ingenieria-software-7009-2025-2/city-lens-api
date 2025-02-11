7d39d695747cc4dd1ba5cee3bab7fa2193a250cd
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-02-11 16:59:33 -0600

    feat: Archivos de Spring Initializer base.

---
+++ src/main/kotlin/com/aperture_science/city_lens_api/CityLensApiApplication.kt
@@ -0,0 +1,11 @@
+package com.aperture_science.city_lens_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CityLensApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CityLensApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/aperture_science/city_lens_api/CityLensApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aperture_science.city_lens_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CityLensApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
