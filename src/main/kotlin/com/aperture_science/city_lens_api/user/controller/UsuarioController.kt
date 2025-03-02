cfc36236ea4e5785b01cb82e2a32decc8d08166d
Edgar Montelongo <edmont@ciencias.unam.mx>
2025-03-02 17:05:53 -0600

    New Definitions of endpoints and data structures, matching the database planning.

--- src/main/kotlin/com/aperture_science/city_lens_api/user/controller/UsuarioController.kt
+++ src/main/kotlin/com/aperture_science/city_lens_api/user/controller/UsuarioController.kt
@@ -2,30 +2,60 @@ package com.aperture_science.city_lens_api.user.controller
 
 import com.aperture_science.city_lens_api.UsuarioRegisterBody
 import com.aperture_science.city_lens_api.user.controller.body.UsuarioLoginBody
+import com.aperture_science.city_lens_api.user.controller.body.UsuarioLoginOutputBody
+import com.aperture_science.city_lens_api.user.repository.entity.SessionToken
 import com.aperture_science.city_lens_api.user.repository.entity.Usuario
+import com.aperture_science.city_lens_api.util.EntityManagerFactoryInstance
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import com.aperture_science.city_lens_api.util.HashUtil
+import jakarta.persistence.EntityManager
 
 @RestController
 @RequestMapping
 class UsuarioController {
 
+
     @PostMapping("/v1/users/login")
-    fun Login(@RequestBody userCredentials: UsuarioLoginBody): ResponseEntity<Usuario> {
-        val loginUser = Usuario(
-            email = userCredentials.email,
-            password = userCredentials.password
+    fun Login(@RequestBody userCredentials: UsuarioLoginBody): ResponseEntity<UsuarioLoginOutputBody> {
+        val em= getEntityManager()
+        //query to database to get the user
+        val loginUser = em.createQuery("SELECT u FROM Usuario u WHERE u.email = :email", Usuario::class.java)
+            .setParameter("email", userCredentials.email)
+            .singleResult
+
+        //autentication of the user, if the code passes this point, the password is correct
+        if (loginUser.password != HashUtil.hash(userCredentials.password)) {
+            em.close()
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        }
+        //create the token and return the answer
+        val response = UsuarioLoginOutputBody(
+            token= SessionToken.createToken(loginUser).token,
+            user= loginUser
         )
-        return ResponseEntity.ok(loginUser)
+        //Save the token in the database
+        em.persist(response.token)
+        em.transaction.commit()
+        em.close()
+        //finally return the response
+        return ResponseEntity.ok(response)
     }
     @PostMapping("/v1/users/register")
-    fun Rogin(@RequestBody userCredentials: UsuarioRegisterBody): ResponseEntity<Usuario> {
+    fun Register(@RequestBody userCredentials: UsuarioRegisterBody): ResponseEntity<Usuario> {
         val loginUser = Usuario(
-            name = userCredentials.name,
+            firstName = userCredentials.first_name,
+            lastName = userCredentials.last_name,
             email = userCredentials.email,
-            password = userCredentials.password
+            password= HashUtil.hash(userCredentials.password),
         )
+        //Due to possible multithreading, we need to create a new EntityManager instance
+        val entityManager= getEntityManager()
+        entityManager.persist(loginUser)
+        entityManager.transaction.commit()
+        entityManager.close()
+
         return ResponseEntity.ok(loginUser)
     }
     @PostMapping("/v1/users/logout")
@@ -35,11 +65,13 @@ class UsuarioController {
     @GetMapping("/v1/users/me")
     fun GetMyUser(): ResponseEntity<Usuario> {
         return ResponseEntity.ok( Usuario(
-            name = "Paquito",
+            firstName = "Paquito",
             email = "paquito@example.com",
             password="123456",
-            token="0"
         )
         )
+    }
+    private fun getEntityManager(): EntityManager {
+        return EntityManagerFactoryInstance.entityManagerFactory!!.createEntityManager()
     }
 }
--- src/main/kotlin/com/aperture_science/city_lens_api/user/repository/entity/Usuario.kt
+++ src/main/kotlin/com/aperture_science/city_lens_api/user/repository/entity/Usuario.kt
@@ -1,12 +1,20 @@
 package com.aperture_science.city_lens_api.user.repository.entity
 
-import jakarta.persistance.*
+import com.aperture_science.city_lens_api.util.HashUtil
+import jakarta.persistence.*
 
 @Entity
 @Table()
 data class Usuario (
-    val name: String = "",
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
     val email: String = "",
-    val password: String = "",
-    val token: String = "",
+    @Column(name = "first_name")
+    val firstName: String,
+    @Column(name = "last_name")
+    val lastName: String = "",
+    @Column(name = "password")
+    var password: String="",
+
 )
