aa10c25b7bafc70cb2f9a15492aaf257f48f0259
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-03-02 13:42:53 -0600

    fix: falta de atributo 'password' en la tabla users

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE User (
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(500) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
     role VARCHAR(20) CHECK (role IN ('user', 'moderator', 'admin')) NOT NULL,
     creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     lastLogin TIMESTAMP
