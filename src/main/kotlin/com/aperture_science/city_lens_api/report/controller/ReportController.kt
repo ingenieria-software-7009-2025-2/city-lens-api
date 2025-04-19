cc8e71121834603c65b8c9cec2304039c2cfa112
Edgar Montelongo <edmont@ciencias.unam.mx>
2025-04-19 04:05:47 -0600

    Quitado /api de la dirección de ReportController

--- src/main/kotlin/com/aperture_science/city_lens_api/report/controller/ReportController.kt
+++ src/main/kotlin/com/aperture_science/city_lens_api/report/controller/ReportController.kt
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 @RestController
-@RequestMapping("/api")
+@RequestMapping()
 class ReportController{
     @PostMapping("/v1/report/create")
     fun createReport(@RequestBody reportCreateBody: ReportCreateBody, request: HttpServletRequest ): ResponseEntity<Any> {
